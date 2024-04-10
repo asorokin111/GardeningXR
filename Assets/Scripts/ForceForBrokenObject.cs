0b72e8b63d29d86146784adbbcaaea6a98230158
Andy <andy.tsl@pm.me>
2024-04-10 15:30:58 +0300

    2 pots

---
+++ Assets/Scripts/ForceForBrokenObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ForceForBrokenObject : MonoBehaviour
+{
+    private List<Rigidbody> _objectParts;
+
+    private void Awake()
+    {
+        _objectParts = GetComponentsInChildren<Rigidbody>().ToList();
+    }
+
+    public void AddForceToAllParts(Vector3 velocity)
+    {
+        foreach (var part in _objectParts)
+        {
+            part.velocity = velocity;
+        }
+    }
+}
