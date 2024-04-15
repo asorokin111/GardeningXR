40a9f164d0a3c7827bf9c9c06ffd93fddfe8b158
asorokin111 <129036204+asorokin111@users.noreply.github.com>
2024-04-15 14:42:22 +0300

    Ported sponge over from VR project

---
+++ Assets/Scripts/SpongeInsert.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+namespace Gardening
+{
+    /// <summary>
+    /// Class that handles inserting flowers into floral sponges
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class SpongeInsert : MonoBehaviour
+    {
+        [SerializeField]
+        private XRGrabInteractable _interactable;
+        private bool _isAnchored = false;
+        private Rigidbody _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _interactable = GetComponent<XRGrabInteractable>();
+            //_interactable.selectEntered.AddListener(Unanchor);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.collider.CompareTag("Sponge") || _isAnchored) return;
+            AnchorTo(other);
+        }
+
+        //private Vector3 GetAverageOfContactPoints(ContactPoint[] points)
+        //{
+        //    Vector3 sum = Vector3.zero;
+        //    Array.ForEach(points, delegate (ContactPoint c) { sum += c.point; });
+        //    return sum / points.Length;
+        //}
+
+        private void AnchorTo(Collision other)
+        {
+            _interactable.enabled = false;
+            _rb.isKinematic = true;
+            GetComponent<Collider>().enabled = false;
+
+            //Vector3 averageOfPoints = GetAverageOfContactPoints(other.contacts);
+            //transform.SetParent(other.transform, true);
+            //transform.SetPositionAndRotation(averageOfPoints, Quaternion.FromToRotation(transform.up, -other.GetContact(0).normal));
+            transform.rotation = Quaternion.FromToRotation(transform.up, -other.GetContact(0).normal);
+
+            _isAnchored = true;
+        }
+
+        //        private void Unanchor(SelectEnterEventArgs args)
+        //        {
+        //#pragma warning disable 0252
+        //            // Comparing references here, need to check if this actually works
+        //            if (args.interactableObject != _interactable) { Debug.Log("Interactable object is not equal to interactable"); return; };
+        //#pragma warning restore
+        //            _isAnchored = false;
+        //            transform.SetParent(null, true);
+        //            GetComponent<Collider>().enabled = true;
+        //            _rb.isKinematic = false;
+        //        }
+    }
+
+}
