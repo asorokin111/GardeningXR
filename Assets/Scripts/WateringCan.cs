552465a2bd96bf9702608aaae165c902f7624e4e
asorokin111 <129036204+asorokin111@users.noreply.github.com>
2024-04-18 12:24:25 +0300

    Fixed watering can rotation oddities

--- Assets/Scripts/WateringCan.cs
+++ Assets/Scripts/WateringCan.cs
@@ -1,12 +1,7 @@
-using UnityEngine;
-
 public class WateringCan : ParticleDropper
 {
     protected override bool CheckTilt()
     {
-        var angles = UnityEditor.TransformUtils.GetInspectorRotation(transform);
-        int dropAngle = 50;
-        int modulo = 360 - dropAngle;
-        return Mathf.Abs(angles.x) % modulo < dropAngle;
+        return _particleSystem.transform.position.y < transform.position.y;
     }
 }
