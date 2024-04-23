4858fa529544e061d89f7b19bbd7b0a63d71c544
Kameji <diman567tak@gmail.com>
2024-04-23 14:16:23 +0300

    Now working

--- Assets/Scripts/GrassPlacingRetest.cs
+++ Assets/Scripts/GrassPlacingRetest.cs
@@ -15,62 +15,12 @@ public class GrassPlacingRetest : MonoBehaviour
 {
     [SerializeField]                private XRRayInteractor      interactor;
     [SerializeField]                private ARAnchorManager      anchorManager;
-    [SerializeField]                private InputActionReference drawGrassButton;
     [GetComponent] [SerializeField] private GrassRender          grassRender;
     [SerializeField]                private ARPlaneManager       planeManager;
 
-    private float       _deltaTime;
-    private IEnumerator _coroutine;
-
-    private bool _canDraw;
-    
     private void OnEnable()
     {
-        drawGrassButton.action.performed += (ctx) => Reenable();
-        _coroutine                       =  AnchorCoroutine();
         planeManager.trackablesChanged.AddListener(PlaceRandomGrass);
-
-    }
-
-    private void Reenable()
-    {
-        SpawnAnchor();
-        _canDraw = !_canDraw;
-        
-        if (_canDraw)
-        {
-            StartCoroutine(_coroutine);
-        }
-        else
-        {
-            StopCoroutine(_coroutine);
-        }
-    }
-    
-
-    private IEnumerator AnchorCoroutine()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(0.1f);
-            SpawnAnchor();   
-        }
-    }
-
-    private async void SpawnAnchor()
-    {
-        interactor.TryGetCurrent3DRaycastHit(out RaycastHit hit);
-
-        Pose hitPos = new Pose(hit.point, Quaternion.LookRotation(hit.normal));
-
-        if (!hit.collider.gameObject.TryGetComponent(out ARPlane plane)) return;
-        if (plane.classifications != PlaneClassifications.DoorFrame) return; ;
-
-        var  result  = await anchorManager.TryAddAnchorAsync(hitPos);
-        bool success = result.TryGetResult(out ARAnchor anchor);
-
-        if (!success) return;
-        grassRender.PlaceObject(anchor.pose.position, anchor.pose.rotation);
     }
 
     private void PlaceRandomGrass(ARTrackablesChangedEventArgs<ARPlane> args)
