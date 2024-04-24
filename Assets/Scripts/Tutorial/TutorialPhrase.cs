b0efab47c25419b95c077dca4c3a7894bbe4f94c
Kameji <diman567tak@gmail.com>
2024-04-24 09:02:30 +0300

    Added tutorial text + small sound fixes

--- Assets/Scripts/Tutorial/TutorialPhrase.cs
+++ Assets/Scripts/Tutorial/TutorialPhrase.cs
@@ -1,6 +1,9 @@
+using UnityEngine;
+
 [System.Serializable]
 public struct TutorialPhrase 
 {
+    [TextArea(1, 3)]
     public string text;
     public float phraseTypingDuration;
     public bool isFillerText; // Filler == not manually triggered
