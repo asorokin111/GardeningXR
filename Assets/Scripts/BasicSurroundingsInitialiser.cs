8903af8236f9fc005c33a80c643f74c63ee87d17
asorokin111 <129036204+asorokin111@users.noreply.github.com>
2024-05-02 15:00:00 +0300

    Replaced the non-working spawner with a really simple one for the time being

--- Assets/Scripts/BasicSurroundingsInitialiser.cs
+++ Assets/Scripts/BasicSurroundingsInitialiser.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
@@ -32,29 +31,13 @@ public class BasicSurroundingsInitialiser : MonoBehaviour
             SpawnAnchor(null);
     }
 
-    public async void SpawnAnchor(BaseInteractionEventArgs _)
+    private void SpawnAnchor(BaseInteractionEventArgs _)
     {
-        Debug.Log("SpawnAnchor called");
         interactor.TryGetCurrent3DRaycastHit(out RaycastHit hit);
 
         if (hit.collider == null) return;
-        Debug.Log("Hit collider check passed");
-        var ob = Instantiate(prefab, hit.collider.transform.position, Quaternion.identity);
-        Debug.Log("Player position: " + GameObject.FindWithTag("Player").transform.position);
-        Debug.Log("Prefab position: " + ob.transform.position);
+
+        Instantiate(prefab, hit.collider.transform.position, Quaternion.identity);
         _hasBeenSpawned = true;
-
-        var rotation = hit.normal != Vector3.zero ? Quaternion.LookRotation(hit.normal) : Quaternion.identity;
-        Pose hitPos = new Pose(hit.point, rotation);
-
-        if (!hit.collider.gameObject.TryGetComponent(out ARPlane plane)) return;
-        if (plane.classifications != PlaneClassifications.DoorFrame) return;
-
-        var result = await anchorManager.TryAddAnchorAsync(hitPos);
-        bool success = result.TryGetResult(out ARAnchor anchor);
-
-        if (!success) return;
-        Debug.Log("Success = true");
-        
     }
 }
